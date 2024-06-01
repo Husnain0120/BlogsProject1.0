f05eaaaf705d3bb8a511a5927f595d9a1b1abd65
Muhammad Husnain Rashid <muhammadhusnainrashid.bs@gmail.com>
2024-06-01 09:12:27 +0500

    secure data

--- Blogs-Website/src/Components/blogPostCard/BlogPostCard.jsx
+++ Blogs-Website/src/Components/blogPostCard/BlogPostCard.jsx
@@ -83,13 +83,289 @@ function BlogPostCard() {
                 })}
               </>
               :
-              <div>
-              <svg 
-              preserveAspectRatio="xMidYMid meet" viewBox="0 0 187.3 93.7" height="300px" width="400px">
-            <path d="M93.9,46.4c9.3,9.5,13.8,17.9,23.5,17.9s17.5-7.8,17.5-17.5s-7.8-17.6-17.5-17.5c-9.7,0.1-13.3,7.2-22.1,17.1 				c-8.9,8.8-15.7,17.9-25.4,17.9s-17.5-7.8-17.5-17.5s7.8-17.5,17.5-17.5S86.2,38.6,93.9,46.4z" stroke-miterlimit="10" stroke-linejoin="round" stroke-linecap="round" stroke-width="4" fill="none" id="outline" stroke="#4E4FEB"></path>
-            <path d="M93.9,46.4c9.3,9.5,13.8,17.9,23.5,17.9s17.5-7.8,17.5-17.5s-7.8-17.6-17.5-17.5c-9.7,0.1-13.3,7.2-22.1,17.1 				c-8.9,8.8-15.7,17.9-25.4,17.9s-17.5-7.8-17.5-17.5s7.8-17.5,17.5-17.5S86.2,38.6,93.9,46.4z" stroke-miterlimit="10" stroke-linejoin="round" stroke-linecap="round" stroke-width="4" stroke="#4E4FEB" fill="none" opacity="0.05" id="outline-bg"></path>
-          </svg>
-          </div>
+              <>
+              <div
+              class="max-w-sm mx-10 mb-5  p-4 border border-gray-200 rounded shadow animate-pulse md:p-6 dark:border-gray-400"
+            >
+              <div
+                class="flex  items-center justify-center h-48 mb-4 bg-gray-300 rounded dark:bg-gray-400"
+              >
+                <svg
+                  viewBox="0 0 16 20"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                  className="w- h-10 text-gray-200 dark:text-gray-600"
+                >
+                  <path
+                    d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z"
+                  ></path>
+                  <path
+                    d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z"
+                  ></path>
+                </svg>
+              </div>
+              <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-400 w-72 mb-4"></div>
+              <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-400 mb-2.5"></div>
+              <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-400 mb-2.5"></div>
+              <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-400"></div>
+              <div class="flex items-center mt-4">
+                {/* <svg
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                  class="w-10 h-10 me-3 text-gray-200 dark:text-gray-400"
+                >
+                  <path
+                    d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"
+                  ></path>
+                </svg> */}
+                <div>
+                  <div
+                    class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-400 w-32 mb-2"
+                  ></div>
+                  <div class="w-48 h-2 bg-gray-200 rounded-full dark:bg-gray-400"></div>
+                </div>
+              </div>
+              <span class="sr-only">Loading...</span>
+            </div>
+            {/* 2 */}
+            <div
+              class="max-w-sm mx-10 mb-5  p-4 border  border-gray-200 rounded shadow animate-pulse md:p-6 dark:border-gray-400"
+            >
+              <div
+                class="flex items-center  justify-center h-48 mb-4 bg-gray-300 rounded dark:bg-gray-400"
+              >
+                <svg
+                  viewBox="0 0 16 20"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                  class="w-10 h-10 text-gray-200 dark:text-gray-600"
+                >
+                  <path
+                    d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z"
+                  ></path>
+                  <path
+                    d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z"
+                  ></path>
+                </svg>
+              </div>
+              <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-400 w-72 mb-4"></div>
+              <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-400 mb-2.5"></div>
+              <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-400 mb-2.5"></div>
+              <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-400"></div>
+              <div class="flex items-center mt-4">
+                {/* <svg
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                  class="w-10 h-10 me-3 text-gray-200 dark:text-gray-400"
+                >
+                  <path
+                    d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"
+                  ></path>
+                </svg> */}
+                <div>
+                  <div
+                    class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-400 w-32 mb-2"
+                  ></div>
+                  <div class="w-48 h-2 bg-gray-200 rounded-full dark:bg-gray-400"></div>
+                </div>
+              </div>
+              <span class="sr-only">Loading...</span>
+            </div>
+            {/* 3 */}
+            <div
+              class="max-w-sm mx-10 mb-5  p-4 border border-gray-200 rounded shadow animate-pulse md:p-6 dark:border-gray-400"
+            >
+              <div
+                class="flex items-center justify-center h-48 mb-4 bg-gray-300 rounded dark:bg-gray-400"
+              >
+                <svg
+                  viewBox="0 0 16 20"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                  class="w-10 h-10 text-gray-200 dark:text-gray-600"
+                >
+                  <path
+                    d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z"
+                  ></path>
+                  <path
+                    d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z"
+                  ></path>
+                </svg>
+              </div>
+              <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-400 w-72 mb-4"></div>
+              <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-400 mb-2.5"></div>
+              <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-400 mb-2.5"></div>
+              <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-400"></div>
+              <div class="flex items-center mt-4">
+                {/* <svg
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                  class="w-10 h-10 me-3 text-gray-200 dark:text-gray-400"
+                >
+                  <path
+                    d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"
+                  ></path>
+                </svg> */}
+                <div>
+                  <div
+                    class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-400 w-32 mb-2"
+                  ></div>
+                  <div class="w-48 h-2 bg-gray-200 rounded-full dark:bg-gray-400"></div>
+                </div>
+              </div>
+              <span class="sr-only">Loading...</span>
+            </div>
+            {/* 4 */}
+            <div
+              class="max-w-sm mx-10 p-4 mb-5  border border-gray-200 rounded shadow animate-pulse md:p-6 dark:border-gray-400"
+            >
+              <div
+                class="flex items-center justify-center h-48 mb-4 bg-gray-300 rounded dark:bg-gray-400"
+              >
+                <svg
+                  viewBox="0 0 16 20"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                  class="w-10 h-10 text-gray-200 dark:text-gray-600"
+                >
+                  <path
+                    d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z"
+                  ></path>
+                  <path
+                    d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z"
+                  ></path>
+                </svg>
+              </div>
+              <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-400 w-72 mb-4"></div>
+              <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-400 mb-2.5"></div>
+              <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-400 mb-2.5"></div>
+              <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-400"></div>
+              <div class="flex items-center mt-4">
+                {/* <svg
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                  class="w-10 h-10 me-3 text-gray-200 dark:text-gray-400"
+                >
+                  <path
+                    d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"
+                  ></path>
+                </svg> */}
+                <div>
+                  <div
+                    class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-400 w-32 mb-2"
+                  ></div>
+                  <div class="w-48 h-2 bg-gray-200 rounded-full dark:bg-gray-400"></div>
+                </div>
+              </div>
+              <span class="sr-only">Loading...</span>
+            </div>
+            {/* 5 */}
+            <div
+              class="max-w-sm mx-10 p-4 mb-5  border border-gray-200 rounded shadow animate-pulse md:p-6 dark:border-gray-400"
+            >
+              <div
+                class="flex items-center justify-center h-48 mb-4 bg-gray-300 rounded dark:bg-gray-400"
+              >
+                <svg
+                  viewBox="0 0 16 20"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                  class="w-10 h-10 text-gray-200 dark:text-gray-600"
+                >
+                  <path
+                    d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z"
+                  ></path>
+                  <path
+                    d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z"
+                  ></path>
+                </svg>
+              </div>
+              <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-400 w-72 mb-4"></div>
+              <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-400 mb-2.5"></div>
+              <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-400 mb-2.5"></div>
+              <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-400"></div>
+              <div class="flex items-center mt-4">
+                {/* <svg
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                  class="w-10 h-10 me-3 text-gray-200 dark:text-gray-400"
+                >
+                  <path
+                    d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"
+                  ></path>
+                </svg> */}
+                <div>
+                  <div
+                    class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-400 w-32 mb-2"
+                  ></div>
+                  <div class="w-48 h-2 bg-gray-200 rounded-full dark:bg-gray-400"></div>
+                </div>
+              </div>
+              <span class="sr-only">Loading...</span>
+            </div>
+            {/* 6 */}
+            <div
+              class="max-w-sm mx-10 p-4 mb-5  border border-gray-200 rounded shadow animate-pulse md:p-6 dark:border-gray-400"
+            >
+              <div
+                class="flex items-center justify-center h-48 mb-4 bg-gray-300 rounded dark:bg-gray-400"
+              >
+                <svg
+                  viewBox="0 0 16 20"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                  class="w-10 h-10 text-gray-200 dark:text-gray-600"
+                >
+                  <path
+                    d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z"
+                  ></path>
+                  <path
+                    d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z"
+                  ></path>
+                </svg>
+              </div>
+              <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-400 w-72 mb-4"></div>
+              <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-400 mb-2.5"></div>
+              <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-400 mb-2.5"></div>
+              <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-400"></div>
+              <div class="flex items-center mt-4">
+                {/* <svg
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                  class="w-10 h-10 me-3 text-gray-200 dark:text-gray-400"
+                >
+                  <path
+                    d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"
+                  ></path>
+                </svg> */}
+                <div>
+                  <div
+                    class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-400 w-32 mb-2"
+                  ></div>
+                  <div class="w-48 h-2 bg-gray-200 rounded-full dark:bg-gray-400"></div>
+                </div>
+              </div>
+              <span class="sr-only">Loading...</span>
+            </div>
+            </>
             }
           </div>
           
